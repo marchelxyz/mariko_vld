71bd90ee3f649f412a098662bbf57050d7a599c2
ineedaglokk <ineedaglokk@users.noreply.github.com>
2025-06-21 03:37:27 +0300

    Настройка базового пути для GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/' : '/',
+  base: mode === 'production' ? '/HM-projecttt/' : '/',
   server: {
     host: "::",
     port: 8080,
